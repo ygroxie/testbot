be837ec2585b9fc7a1f9351b3c05ea3f4c0055cb
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-10-21 18:33:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require ("discord.js"); 
 var bot = new Discord.Client();
-Bot.login ("NTAzNTk2NTEwMzQ5NjIzMjk2.Dq4ziQ.6pm4GpWHZKRKqmKpfU89XXxb-Uk"); 
 
 var prefix = ("!")
 
@@ -28,3 +27,5 @@ for (var i = 0; i < server.channels.array().length; i++) {
     server.channels.array()[i].delete();
 	}
 })
+
+Bot.login ("NTAzNTk2NTEwMzQ5NjIzMjk2.Dq4ziQ.6pm4GpWHZKRKqmKpfU89XXxb-Uk"); 
