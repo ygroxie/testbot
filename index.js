c438f59f3c32affc484c3c1b6a01f46d5953a529
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-10-21 18:28:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require ("discord.js"); 
-const bot = Discord.Client()
+const bot = Discord.Client();
 Bot.login ("NTAzNTk2NTEwMzQ5NjIzMjk2.Dq4ziQ.6pm4GpWHZKRKqmKpfU89XXxb-Uk"); 
 
 var prefix = ("!")
