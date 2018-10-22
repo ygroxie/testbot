1d4b26ea02529c92c46d94ffccd916d91cc64d51
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-10-22 13:46:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,85 @@
-const Discord = require ("discord.js"); 
+const Discord = require("discord.js");
+
 var bot = new Discord.Client();
 
-var prefix = ("!")
+var prefix = ("/")
 
-bot.on ("ready", function () { 
-console.log ("Ready."); 
-}); 
+bot.on("ready", function() {
+    bot.user.setGame("Minecraft");
+    console.log("Le bot a bien était connecté !");
+});
 
-bot.on ("message", fonction (msg) { 
-if (msg.content.toLowerCase () == "destroy") { 
-for (membres var in msg.guild.members) {members.ban 
-(); 
-} 
-} 
-} )
+bot.on('message', function (message) {
+    if (message.content === '?ping') {
+        message.channel.send('pong !')
+    }
+});
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name', 'Joueur');
+    member.addRole(role)
+})
+
+bot.on('guildMemberAdd', member => {
+    var bvn_embed = new Discord.RichEmbed()
+    .setColor('#E81414')
+    .setImage(member.user.displayAvatarURL)
+    .addField("Bienvenue", `Bienvenue ${member} sur VanaDium nous somme actuellement ${member.guild.memberCount} membres !`)
+    .setFooter(`${member.user.username}`)
+    .setTimestamp()
+    member.guild.channels.find("name", "nouveau").send(bvn_embed)
+})
+
+bot.on('message',(message)=>{
+    if(message.content == "/help") {
+    var embed = new Discord.RichEmbed()
+    .addField("?help","affiche les commandes du bot.")
+    .addField("?kick","Permet de kick un joueur du serveur Discord.")
+    .addField("?ban", "Permet de bannir un Joueur du serveur Discord.")
+    .addField("?ping", "Joue au Ping-Pong avec le bot !")
+    .setColor("D7F705")
+    .setTitle("Guide Commandes !")
+    .setFooter ("(Utile si tu as courtes mémoire ! x) ")
+    message.channel.send(embed)
+    }
+    })
 
 bot.on('message', message => {
     let command = message.content.split(" ")[0];
     const args = message.content.slice(prefix.length).split(/ +/);
     command = args.shift().toLowerCase();
 
+    if (command === "kick") {
+        if(!message.channel.permissionsFor(message.member).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Tu n'as pas la permission de faire cette commande. Désolé !").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Merci de mentionner l'utilisateur à expulser.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utilisateur est introuvable ou impossible à expulser")
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user} a été expulsé avec succès !`).catch(console.error);
+            message.guild.channels.find("name", "logs").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**`);
+        }).catch(console.error)
+        
+}
 
-	if (command === "kick") {
-	var server = Client.guilds.get('503557304881250311');
-for (var i = 0; i < server.channels.array().length; i++) {
-    server.channels.array()[i].delete();
-	}
-})
+if (command === "ban") {
+    if (!message.channel.permissionsFor(message.member).hasPermission("BAN_MEMBERS")) {
+        return message.reply("Tu n'as pas la permission de faire cette commande. Désolé !").catch(console.error);
+    }
+    const member = message.mentions.members.first();
+    if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
+    member.ban().then(member => {
+        message.reply(`${member.user} a été banni avec succès !`).catch(console.error);
+        message.guild.channels.find("name", "logs").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`);
+    }).catch(console.error)
+}})
 
-Bot.login ("NTAzNTk2NTEwMzQ5NjIzMjk2.Dq4ziQ.6pm4GpWHZKRKqmKpfU89XXxb-Uk"); 
+bot.login(process.env.TOKEN)
